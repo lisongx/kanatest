9a77f9c50ae511f4ef996819bb4c67f7a8bc6501
Li Song <lisong@notimportant.org>
2020-04-14 23:18:16 +0100

    get tachyons working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.scss";
+import 'tachyons/css/tachyons.css'
 
 import Kana from './components/Kana';
 
--- src/components/Kana/index.js
+++ src/components/Kana/index.js
@@ -5,7 +5,7 @@ export default class Kana extends React.PureComponent {
     render() {
         const { char } = this.props;
 
-        return <div class="kana-item">
+        return <div class="f-headline kana-item">
             {char}
         </div>
     }
